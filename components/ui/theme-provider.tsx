a679d17ffa65246a0c0969131023c7fb05b3be59
jwalla7 <jwalla7@protonmail.com>
2023-05-27 14:47:10 -0500

    theme provider

--- components/ui/theme-provider.tsx
+++ components/ui/theme-provider.tsx
@@ -1,3 +1,14 @@
 "use client";
 
 import * as React from "react";
+import { ThemeProvider as PrimeThemeProvider } from "@primer/react";
+import { ThemeProviderProps } from "@primer/react";
+
+/* 
+ThemeProvider, ThemeProviderProps
+https://github.com/primer/react/blob/main/src/ThemeProvider.tsx 
+*/
+
+export function ThemeProvider({ ...props }: ThemeProviderProps) {
+  return <PrimeThemeProvider {...props}></PrimeThemeProvider>;
+}

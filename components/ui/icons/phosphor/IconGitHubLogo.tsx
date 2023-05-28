3f420f3acf03a4c5e86e82eafab43c1d3664ae1e
jwalla7 <jwalla7@protonmail.com>
2023-05-28 18:03:09 -0500

    icon envelope simple

--- components/ui/icons/phosphor/IconGitHubLogo.tsx
+++ components/ui/icons/phosphor/IconGitHubLogo.tsx
@@ -6,8 +6,8 @@ https://react.dev/reference/react/memo
 */
 import { SVGProps, memo } from "react";
 
-export const IconGitHub = memo<React.JSX.IntrinsicElements["svg"]>(
-    function IconGitHub(props: SVGProps<SVGSVGElement>) {
+export const IconGitHubLogo = memo<React.JSX.IntrinsicElements["svg"]>(
+    function IconGitHubLogo(props: SVGProps<SVGSVGElement>) {
         return (
             <svg
                 xmlns="http://www.w3.org/2000/svg"

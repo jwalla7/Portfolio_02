3b30bb025588fe998bede60ed90630311202d481
jwalla7 <jwalla7@protonmail.com>
2023-05-28 23:37:18 -0500

    metadata robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,9 @@
 import { MetadataRoute } from "next";
 
+/*
+designates which URLs search engine crawlers can access on site.
+ */
+
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: {

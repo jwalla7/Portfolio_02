6a9cef3088420da163ce3fb21fe85060b0654453
jwalla7 <jwalla7@protonmail.com>
2023-05-31 18:28:49 -0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
 };
 
 export default withContentlayer(nextConfig);

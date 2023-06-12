8dec8605e9256e2cb76281441010b8f93bd2493b
jwalla7 <jwalla7@protonmail.com>
2023-06-11 19:28:50 -0500

    theme

---
+++ components/ui/theme/theme-toggle.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import * as React from "react";
+import { useTheme } from "next-themes";
+
+// TODO import buttons menu,content,items,trigger
+
+export function ThemeToggle() {
+    const { setTheme } = useTheme();
+    return <></>;
+}

158d074d7f50075b2e5bd47845bddc9f4b0e7798
jwalla7 <jwalla7@protonmail.com>
2023-06-12 19:57:03 -0500

    font

--- app/(about)/music/page.tsx
+++ app/(about)/music/page.tsx
@@ -2,7 +2,16 @@ import { cn } from "@/lib/utils";
 import { robotoRegular } from "@/design/fontDefaults";
 
 const MusicPage = () => {
-    return <h1 className={cn(robotoRegular.className)}>My music</h1>;
+    return (
+        <h1
+            className={cn(
+                "mt-3 text-3xl text-orange-50",
+                robotoRegular.className
+            )}
+        >
+            My music
+        </h1>
+    );
 };
 
 export default MusicPage;

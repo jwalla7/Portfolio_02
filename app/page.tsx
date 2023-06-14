d450e5db6ffc94fefbb80aa5d8c36d596bdae4f9
jwalla7 <jwalla7@protonmail.com>
2023-06-14 13:27:17 -0500

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { PhosphorIcons } from "@/components/ui/icons";
+import { IconHouse } from "@/components/ui/icons/phosphor/IconHouse";
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+
 export default function Main() {
-    return <div>Main page</div>;
+    return (
+        <div className="flex gap-6">
+            <Link href="/" className="items-center space-x-2 ">
+                <PhosphorIcons.arrowCircleLeft iconDirection="0_rotation" />
+                <span className="font-bold text-yellow-200">
+                    {siteConfig.copyright}
+                </span>
+            </Link>
+        </div>
+    );
 }

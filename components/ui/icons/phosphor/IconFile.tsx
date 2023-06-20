02d556781ee42665fb2df1eb7dcf7f1a02afc07c
jwalla7 <jwalla7@protonmail.com>
2023-06-19 20:33:11 -0400

    icon file

--- components/ui/icons/phosphor/IconFile.tsx
+++ components/ui/icons/phosphor/IconFile.tsx
@@ -2,25 +2,31 @@
 memo bypasses re-rendering a component if its props are unchanged.
 https://react.dev/reference/react/memo
 */
-import { SVGProps, memo } from "react";
+import { cn } from "@/lib/utils";
+import React, { SVGProps, memo } from "react";
 
-export const IconFile = memo<React.JSX.IntrinsicElements["svg"]>(
-    function IconFile(props: SVGProps<SVGSVGElement>) {
-        return (
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="phosphorIcon-file"
-                viewBox="0 0 256 256"
-                {...props}
-            >
-                <path
-                    fill="currentColor"
-                    d="M216.49,79.52l-56-56A12,12,0,0,0,152,20H56A20,20,0,0,0,36,40V216a20,20,0,0,0,20,20H200a20,20,0,0,0,20-20V88A12,12,0,0,0,216.49,79.52ZM160,57l23,23H160ZM60,212V44h76V92a12,12,0,0,0,12,12h48V212Z"
-                ></path>
-            </svg>
-        );
-    }
-);
+export const IconFile = memo<React.ComponentProps<"svg">>(function IconFile(
+    /** Accessibility to other SVG props */
+    props: SVGProps<SVGSVGElement>,
+    { className }
+) {
+    /** Create className values */
+    const variants = cn("transition duration-100 ease-in", className);
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fab"
+            data-icon="phosphorIcon-file"
+            viewBox="0 0 256 256"
+            className={variants}
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                d="M216.49,79.52l-56-56A12,12,0,0,0,152,20H56A20,20,0,0,0,36,40V216a20,20,0,0,0,20,20H200a20,20,0,0,0,20-20V88A12,12,0,0,0,216.49,79.52ZM160,57l23,23H160ZM60,212V44h76V92a12,12,0,0,0,12,12h48V212Z"
+            ></path>
+        </svg>
+    );
+});

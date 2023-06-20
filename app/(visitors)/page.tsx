ede5aa1bf9ffcb30a70bb8c607428c5113d8e04e
jwalla7 <jwalla7@protonmail.com>
2023-06-20 13:04:15 -0400

    update

---
+++ app/(visitors)/page.tsx
@@ -0,0 +1,7 @@
+export default function VisitorsPage() {
+    return (
+        <>
+            <div>hello</div>
+        </>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,9 @@ module.exports = {
 
     theme: {
         extend: {
+            screens: {
+                // Add new breakpoints an addition to default breakpoints
+            },
             // backgroundImage: {
             //     "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
             //     "gradient-conic":
@@ -38,6 +41,25 @@ module.exports = {
                     300: "rgba(250, 250, 250, 0.34)",
                 },
                 blueviolet: "#9747ff",
+            },
+            gradientColorStopPositions: {
+                100: "100%",
+                99.2: "99.2%",
+                96.5: "96.5%",
+                91.8: "91.8%",
+                85: "85.5%",
+                80: "80%",
+                77: "77%",
+                66.7: "66.7%",
+                55.7: "55.7%",
+                44.3: "44.3%",
+                43: "43%",
+                33.3: "33.3%",
+                23: "23%",
+                14.5: "14.5%",
+                8.2: "8.2%",
+                3.5: "3.5%",
+                0.8: "0.8%",
             },
             borderRadius: {
                 "8xs": "5px",

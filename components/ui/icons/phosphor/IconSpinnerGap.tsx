f3ec8e953aed991fef2b53ac842b862fa5a18461
jwalla7 <jwalla7@protonmail.com>
2023-06-19 21:07:58 -0400

    icon spinnger gap

--- components/ui/icons/phosphor/IconSpinnerGap.tsx
+++ components/ui/icons/phosphor/IconSpinnerGap.tsx
@@ -2,25 +2,44 @@
 memo bypasses re-rendering a component if its props are unchanged.
 https://react.dev/reference/react/memo
 */
+import { cn } from "@/lib/utils";
+import { IconDirection } from "@/types/icon";
 import { SVGProps, memo } from "react";
 
-export const IconSpinnerGap = memo<React.JSX.IntrinsicElements["svg"]>(
-    function IconSpinnerGap(props: SVGProps<SVGSVGElement>) {
-        return (
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="phosphorIcon-spinnerGap"
-                viewBox="0 0 256 256"
-                {...props}
-            >
-                <path
-                    fill="currentColor"
-                    d="M140,32V64a12,12,0,0,1-24,0V32a12,12,0,0,1,24,0Zm84,84H192a12,12,0,0,0,0,24h32a12,12,0,0,0,0-24Zm-42.26,48.77a12,12,0,1,0-17,17l22.63,22.63a12,12,0,0,0,17-17ZM128,180a12,12,0,0,0-12,12v32a12,12,0,0,0,24,0V192A12,12,0,0,0,128,180ZM74.26,164.77,51.63,187.4a12,12,0,0,0,17,17l22.63-22.63a12,12,0,1,0-17-17ZM76,128a12,12,0,0,0-12-12H32a12,12,0,0,0,0,24H64A12,12,0,0,0,76,128ZM68.6,51.63a12,12,0,1,0-17,17L74.26,91.23a12,12,0,0,0,17-17Z"
-                ></path>
-            </svg>
-        );
+export const IconSpinnerGap = memo<
+    React.ComponentProps<"svg"> & {
+        iconDirection: IconDirection;
     }
-);
+>(function IconSpinnerGap(
+    /** Accessibility to other SVG props */
+    props: SVGProps<SVGSVGElement>,
+    { iconDirection = "0_rotation", className }
+) {
+    const variants = cn(
+        "transition duration-100 ease-in",
+        {
+            "rotate-0": iconDirection === "0_rotation",
+            "rotate-90": iconDirection === "90_rotation",
+            "rotate-180": iconDirection === "180_rotation",
+            "-rotate-90": iconDirection === "-90_rotation",
+        },
+        className
+    );
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fab"
+            data-icon="phosphorIcon-spinnerGap"
+            viewBox="0 0 256 256"
+            className={variants}
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                d="M140,32V64a12,12,0,0,1-24,0V32a12,12,0,0,1,24,0Zm84,84H192a12,12,0,0,0,0,24h32a12,12,0,0,0,0-24Zm-42.26,48.77a12,12,0,1,0-17,17l22.63,22.63a12,12,0,0,0,17-17ZM128,180a12,12,0,0,0-12,12v32a12,12,0,0,0,24,0V192A12,12,0,0,0,128,180ZM74.26,164.77,51.63,187.4a12,12,0,0,0,17,17l22.63-22.63a12,12,0,1,0-17-17ZM76,128a12,12,0,0,0-12-12H32a12,12,0,0,0,0,24H64A12,12,0,0,0,76,128ZM68.6,51.63a12,12,0,1,0-17,17L74.26,91.23a12,12,0,0,0,17-17Z"
+            ></path>
+        </svg>
+    );
+});

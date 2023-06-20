12edfbf241ce9f2ca45814303878a84e12520490
jwalla7 <jwalla7@protonmail.com>
2023-06-19 21:30:28 -0400

    theme toggle

--- components/ui/theme/ThemeToggle.tsx
+++ components/ui/theme/ThemeToggle.tsx
@@ -2,10 +2,26 @@
 
 import * as React from "react";
 import { useTheme } from "next-themes";
+import { Button } from "../button/Button";
+import { IconMoon } from "../icons/phosphor/IconMoon";
+import { IconSun } from "../icons/phosphor/IconSun";
 
 // TODO import buttons menu,content,items,trigger
 
 export function ThemeToggle() {
-    const { setTheme } = useTheme();
-    return <></>;
+    const { theme, setTheme } = useTheme();
+    const { _, startTransition } = React.useTransition();
+
+    return (
+        <Button
+            variant="secondary"
+            onClick={() => {
+                startTransition(() => {
+                    setTheme(theme === "dark" ? "light" : "dark");
+                });
+            }}
+        >
+            {!theme ? null : theme === "dark" ? <IconMoon /> : <IconSun />}
+        </Button>
+    );
 }

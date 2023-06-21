c2049d05358428bdf0768f7a88c3b6e7f847fefa
jwalla7 <jwalla7@protonmail.com>
2023-06-21 13:06:56 -0400

    popover right

--- components/ui/popover/PopoverRight.tsx
+++ components/ui/popover/PopoverRight.tsx
@@ -6,7 +6,21 @@ const PopoverRight = () => (
     <Popover.Root>
         <Popover.Trigger asChild>
             <button
-                className="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-violet11 bg-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black cursor-default outline-none"
+                className="rounded-full
+                w-[35px]
+                h-[35px]
+                inline-flex
+                items-center
+                justify-center
+                text-violet11
+                bg-white
+                shadow-[0_2px_10px]
+                shadow-blackA7
+                hover:bg-violet3
+                focus:shadow-[0_0_0_2px]
+                focus:shadow-black
+                cursor-default
+                outline-none"
                 aria-label="Update dimensions"
             >
                 {/* <MixerHorizontalIcon /> */}
@@ -14,7 +28,17 @@ const PopoverRight = () => (
         </Popover.Trigger>
         <Popover.Portal>
             <Popover.Content
-                className="rounded p-5 w-[260px] bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+                className="rounded
+                p-5
+                w-[260px] 
+                bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] 
+                focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] 
+                will-change-[transform,opacity]
+                data-[state=open]:data-[side=top]:animate-slideDownAndFade 
+                data-[state=open]:data-[side=right]:animate-slideLeftAndFade
+                data-[state=open]:data-[side=bottom]:animate-slideUpAndFade
+                data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+                side="right"
                 sideOffset={5}
             >
                 <div className="flex flex-col gap-2.5">

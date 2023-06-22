bf159dac11bc84d9417d296279024b4b20da529d
jwalla7 <jwalla7@protonmail.com>
2023-06-22 18:03:40 -0400

    theme toggle group

--- components/ui/theme/ThemeToggleGroup.tsx
+++ components/ui/theme/ThemeToggleGroup.tsx
@@ -32,7 +32,7 @@ const ThemeToggleGroup = React.forwardRef<
                         ) : (
                             <IconSun className="h-9 w-9" />
                         )}
-                        {/* <span className="sr-only">Toggle theme</span> */}
+                        <span className="sr-only">Toggle theme</span>
                         {/* rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 */}
                         {/* rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 */}
                     </Button>

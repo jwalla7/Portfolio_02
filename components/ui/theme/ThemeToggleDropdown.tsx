a05b100830d75e98e7f832c969f1c1b25b7d1235
jwalla7 <jwalla7@protonmail.com>
2023-06-24 16:44:35 -0500

    theme toggle

--- components/ui/theme/ThemeToggleDropdown.tsx
+++ components/ui/theme/ThemeToggleDropdown.tsx
@@ -45,8 +45,6 @@ const ThemeToggleDropdown = React.forwardRef<
                                 <label>Theme</label>
                             </menu>
                         )}
-                        {/* rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 */}
-                        {/* rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 */}
                     </Button>
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Portal>

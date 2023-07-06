1c7de7d52bebeddfdda70dbff6c9912d044d9aad
jwalla7 <jwalla7@protonmail.com>
2023-07-05 20:46:53 -0500

    update

--- app/(main)/main/page.tsx
+++ app/(main)/main/page.tsx
@@ -1,18 +1,34 @@
+"use client";
+
 import { OverylayNav } from "@/components/ui/navigation/web/overlay/OverlayNav/OverylayNav";
 import { OverlayRoot } from "@/components/ui/navigation/web/overlay/OverlayRoot/OverlayRoot";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function MainPage() {
-    const image = "/SKYF-lightmode.jpg";
+    const imageLightMode = "/SKYF-lightmode.jpg";
+    const imageDarkMode = "/SKYF-darkmode.jpg";
+    const { theme, setTheme } = useTheme();
+
     return (
         <div className="absolute left-0 top-0 right-0 bottom-0 min-w-full min-h-screen overflow-x-hidden">
-            <Image
-                alt="background image"
-                width={1920}
-                height={2464}
-                src={image}
-                quality={100}
-            />
+            {theme === "light" ? (
+                <Image
+                    alt="background image"
+                    width={1920}
+                    height={2464}
+                    src={imageLightMode}
+                    quality={100}
+                />
+            ) : (
+                <Image
+                    alt="background image"
+                    width={1920}
+                    height={2464}
+                    src={imageDarkMode}
+                    quality={100}
+                />
+            )}
             {/* <Image  alt="background image" width={1440} height={1848} src={image}  quality={100} /> */}
             {/* Need a trigger context of component */}
             {/* <OverlayRoot> */}

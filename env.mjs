a5a319dba7948d4890eb54f726eb50d8e739c511
jwalla7 <jwalla7@protonmail.com>
2023-07-07 22:31:21 -0500

    update

--- env.mjs
+++ env.mjs
@@ -4,30 +4,27 @@ import { z } from "zod";
 // Create schema, all env variables from .env files need to be included to avoid errors.
 export const env = createEnv({
     server: {
-        NEXTAUTH_URL: z.string().url().optional(),
-        NEXTAUTH_SECRET: z.string().min(1),
-        GITHUB_ID: z.string().min(1),
-        GITHUB_SECRET: z.string().min(1),
+        // NEXTAUTH_URL: z.string().url().optional(),
+        // NEXTAUTH_SECRET: z.string().min(1),
+        // GITHUB_ID: z.string().min(1),
+        // GITHUB_SECRET: z.string().min(1),
         // GITHUB_ACCESS_TOKEN: z.string().min(1),
-        GOOGLE_CLIENT_ID: z.string().min(1),
-        GOOGLE_CLIENT_SECRET: z.string().min(1),
+        // GOOGLE_CLIENT_ID: z.string().min(1),
+        // GOOGLE_CLIENT_SECRET: z.string().min(1),
     },
     client: {
         // NEXT_PUBLIC_URL: z.string().min(1),
     },
     runtimeEnv: {
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-        GITHUB_ID: process.env.GITHUB_ID,
-        GITHUB_SECRET: process.env.GITHUB_SECRET,
+        // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        // GITHUB_ID: process.env.GITHUB_ID,
+        // GITHUB_SECRET: process.env.GITHUB_SECRET,
         // GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-
+        // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        // GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
         // TODO
-
         // DATABASE_URL: process.env.DATABASE_URL,
-
         // SMTP_FROM: process.env.SMTP_FROM,
         // POSTMARK_API_TOKEN: process.env.POSTMARK_API_TOKEN,
         // POSTMARK_SIGN_IN_TEMPLATE: process.env.POSTMARK_SIGN_IN_TEMPLATE,

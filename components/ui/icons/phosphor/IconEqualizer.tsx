4f1ee855e27eb320e1d1819e9ff11717aabbc200
jwalla7 <jwalla7@protonmail.com>
2023-08-04 17:36:17 -0500

    icon equalizer

--- components/ui/icons/phosphor/IconEqualizer.tsx
+++ components/ui/icons/phosphor/IconEqualizer.tsx
@@ -1,12 +1,14 @@
-/*
-memo bypasses re-rendering a component if its props are unchanged.
-https://react.dev/reference/react/memo
-*/
+/**
+ * memo bypasses re-rendering a component if its props are unchanged.
+ * https://react.dev/reference/react/memo
+ */
 import { SVGProps, memo } from "react";
 
 export const IconEqualizer = memo<React.ComponentProps<"svg">>(
     function IconEqualizer(
-        /** Accessibility to other SVG props */
+        /**
+         * Accessibility to other SVG props
+         */
         props: SVGProps<SVGSVGElement>
     ) {
         return (

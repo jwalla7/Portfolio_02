ddbdb622016d1b87cd59bc2bb4d6fe36aefd9c81
jwalla7 <jwalla7@protonmail.com>
2023-08-11 11:11:15 -0500

    icon house

--- components/ui/icons/phosphor/IconHouse.tsx
+++ components/ui/icons/phosphor/IconHouse.tsx
@@ -1,7 +1,8 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
 
 import { SVGProps, memo } from "react";
@@ -9,12 +10,12 @@ import { cn } from "@/lib/utils";
 
 export const IconHouse = memo<React.ComponentProps<"svg">>(function IconHouse(
     /**
-     * Accessibility to other SVG props
+     * @summary Accessibility to other SVG props
      */
-    props: SVGProps<SVGSVGElement>
+    { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
 ) {
     /**
-     * Create custom values for className
+     * @summary Creates custom values for className
      */
     const variants = cn(
         "block flex-grow-0 flex-shrink-0 w-[33.39px] h-[33.39px] relative"
@@ -31,7 +32,8 @@ export const IconHouse = memo<React.ComponentProps<"svg">>(function IconHouse(
             {...props}
         >
             <path
-                fill="currentColor"
+                fill={fill}
+                fillOpacity={fillOpacity}
                 d="M221.56,100.85,141.61,25.38l-.16-.15a19.93,19.93,0,0,0-26.91,0l-.17.15L34.44,100.85A20.07,20.07,0,0,0,28,115.55V208a20,20,0,0,0,20,20H96a20,20,0,0,0,20-20V164h24v44a20,20,0,0,0,20,20h48a20,20,0,0,0,20-20V115.55A20.07,20.07,0,0,0,221.56,100.85ZM204,204H164V160a20,20,0,0,0-20-20H112a20,20,0,0,0-20,20v44H52V117.28l76-71.75,76,71.75Z"
             ></path>
         </svg>

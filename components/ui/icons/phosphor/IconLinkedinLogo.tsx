1cf9b7cf42fa21208c65ca671aafb5c503bdb02f
jwalla7 <jwalla7@protonmail.com>
2023-08-11 11:09:10 -0500

    icon linkedin logo

--- components/ui/icons/phosphor/IconLinkedinLogo.tsx
+++ components/ui/icons/phosphor/IconLinkedinLogo.tsx
@@ -1,16 +1,18 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
+
 import { SVGProps, memo } from "react";
 
 export const IconLinkedinLogo = memo<React.ComponentProps<"svg">>(
     function IconLinkedinLogo(
         /**
-         * Accessibility to other SVG props
+         * @summary Accessibility to other SVG props
          */
-        props: SVGProps<SVGSVGElement>
+        { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
     ) {
         return (
             <svg
@@ -23,7 +25,8 @@ export const IconLinkedinLogo = memo<React.ComponentProps<"svg">>(
                 {...props}
             >
                 <path
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M216,20H40A20,20,0,0,0,20,40V216a20,20,0,0,0,20,20H216a20,20,0,0,0,20-20V40A20,20,0,0,0,216,20Zm-4,192H44V44H212ZM112,176V124a12,12,0,0,1,21.43-7.41A40,40,0,0,1,192,152v24a12,12,0,0,1-24,0V152a16,16,0,0,0-32,0v24a12,12,0,0,1-24,0ZM96,124v52a12,12,0,0,1-24,0V124a12,12,0,0,1,24,0ZM68,80A16,16,0,1,1,84,96,16,16,0,0,1,68,80Z"
                 ></path>
             </svg>

ad9017d4db25db5f7e602b694276b9c80969a7a0
jwalla7 <jwalla7@protonmail.com>
2023-08-11 11:17:31 -0500

    icon envelope simple

--- components/ui/icons/phosphor/IconEnvelopeSimple.tsx
+++ components/ui/icons/phosphor/IconEnvelopeSimple.tsx
@@ -1,7 +1,8 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
 
 import { SVGProps, memo } from "react";
@@ -9,9 +10,9 @@ import { SVGProps, memo } from "react";
 export const IconEnvelopSimple = memo<React.ComponentProps<"svg">>(
     function IconEnvelopSimple(
         /**
-         * Accessibility to other SVG props
+         * @summary Accessibility to other SVG props
          */
-        props: SVGProps<SVGSVGElement>
+        { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
     ) {
         return (
             <svg
@@ -24,7 +25,8 @@ export const IconEnvelopSimple = memo<React.ComponentProps<"svg">>(
                 {...props}
             >
                 <path
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M224,44H32A12,12,0,0,0,20,56V192a20,20,0,0,0,20,20H216a20,20,0,0,0,20-20V56A12,12,0,0,0,224,44ZM193.15,68,128,127.72,62.85,68ZM44,188V83.28l75.89,69.57a12,12,0,0,0,16.22,0L212,83.28V188Z"
                 ></path>
             </svg>

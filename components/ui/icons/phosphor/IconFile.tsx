c3977582539c2e58b91dfc4e6e05b37b12ddd1bb
jwalla7 <jwalla7@protonmail.com>
2023-08-11 11:13:55 -0500

    icon file

--- components/ui/icons/phosphor/IconFile.tsx
+++ components/ui/icons/phosphor/IconFile.tsx
@@ -1,15 +1,17 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
+
 import React, { SVGProps, memo } from "react";
 
 export const IconFile = memo<React.ComponentProps<"svg">>(function IconFile(
     /**
-     * Accessibility to other SVG props
+     * @summary Accessibility to other SVG props
      */
-    props: SVGProps<SVGSVGElement>
+    { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
 ) {
     return (
         <svg
@@ -22,7 +24,8 @@ export const IconFile = memo<React.ComponentProps<"svg">>(function IconFile(
             {...props}
         >
             <path
-                fill="currentColor"
+                fill={fill}
+                fillOpacity={fillOpacity}
                 d="M216.49,79.52l-56-56A12,12,0,0,0,152,20H56A20,20,0,0,0,36,40V216a20,20,0,0,0,20,20H200a20,20,0,0,0,20-20V88A12,12,0,0,0,216.49,79.52ZM160,57l23,23H160ZM60,212V44h76V92a12,12,0,0,0,12,12h48V212Z"
             ></path>
         </svg>

26eb41daff7a6fbdd8379706196eab6e32c6787a
jwalla7 <jwalla7@protonmail.com>
2023-08-11 10:33:11 -0500

    icon sun

--- components/ui/icons/phosphor/IconSun.tsx
+++ components/ui/icons/phosphor/IconSun.tsx
@@ -1,8 +1,10 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
+
 import { SVGProps, memo } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
@@ -13,23 +15,23 @@ export const IconSun = memo<
     }
 >(function IconSun(
     /**
-     * Accessibility to customized attributes setMotion, and className
+     * @summary Accessibility to customized attributes setMotion, className, and to other SVG props
      */
     { setMotion, className },
     /**
-     * Accessibility to other SVG props
+     * @summary Accessibility to other SVG props
      */
-    props: SVGProps<SVGSVGElement>
+    { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
 ) {
     /**
-     * Create custom values for className
+     * @summary Creates custom values for className
      */
     const variants = cn(
         "block flex-grow-0 flex-shrink-0 w-[33.39px] h-[33.39px] relative",
         className
     );
     /**
-     * Establishes which variation of `IconSun` to render
+     * @summary Establishes which variation of `IconSun` to render
      */
     if (setMotion) {
         return (
@@ -45,9 +47,9 @@ export const IconSun = memo<
             >
                 <motion.path
                     /**
-                     * Initial to animate, creates a fade-in-with-rotation animation
+                     * @summary  From initial to animated, creates a fade-in-with-rotation animation
                      *
-                     * https://framer.com/motion/transition/##value-specific-transitions
+                     * @see https://framer.com/motion/transition/##value-specific-transitions
                      */
                     initial={{
                         rotate: 0,
@@ -64,7 +66,8 @@ export const IconSun = memo<
                         duration: 0.55,
                         ease: "anticipate",
                     }}
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M116,32V16a12,12,0,0,1,24,0V32a12,12,0,0,1-24,0Zm80,96a68,68,0,1,1-68-68A68.07,68.07,0,0,1,196,128Zm-24,0a44,44,0,1,0-44,44A44.05,44.05,0,0,0,172,128ZM51.51,68.49a12,12,0,1,0,17-17l-12-12a12,12,0,0,0-17,17Zm0,119-12,12a12,12,0,0,0,17,17l12-12a12,12,0,1,0-17-17ZM196,72a12,12,0,0,0,8.49-3.51l12-12a12,12,0,0,0-17-17l-12,12A12,12,0,0,0,196,72Zm8.49,115.51a12,12,0,0,0-17,17l12,12a12,12,0,0,0,17-17ZM44,128a12,12,0,0,0-12-12H16a12,12,0,0,0,0,24H32A12,12,0,0,0,44,128Zm84,84a12,12,0,0,0-12,12v16a12,12,0,0,0,24,0V224A12,12,0,0,0,128,212Zm112-96H224a12,12,0,0,0,0,24h16a12,12,0,0,0,0-24Z"
                 />
             </motion.svg>
@@ -83,7 +86,8 @@ export const IconSun = memo<
                 {...props}
             >
                 <path
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M116,32V16a12,12,0,0,1,24,0V32a12,12,0,0,1-24,0Zm80,96a68,68,0,1,1-68-68A68.07,68.07,0,0,1,196,128Zm-24,0a44,44,0,1,0-44,44A44.05,44.05,0,0,0,172,128ZM51.51,68.49a12,12,0,1,0,17-17l-12-12a12,12,0,0,0-17,17Zm0,119-12,12a12,12,0,0,0,17,17l12-12a12,12,0,1,0-17-17ZM196,72a12,12,0,0,0,8.49-3.51l12-12a12,12,0,0,0-17-17l-12,12A12,12,0,0,0,196,72Zm8.49,115.51a12,12,0,0,0-17,17l12,12a12,12,0,0,0,17-17ZM44,128a12,12,0,0,0-12-12H16a12,12,0,0,0,0,24H32A12,12,0,0,0,44,128Zm84,84a12,12,0,0,0-12,12v16a12,12,0,0,0,24,0V224A12,12,0,0,0,128,212Zm112-96H224a12,12,0,0,0,0,24h16a12,12,0,0,0,0-24Z"
                 ></path>
             </svg>

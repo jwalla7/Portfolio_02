133e649feb32414d4c04560aa674369a60c305c5
jwalla7 <jwalla7@protonmail.com>
2023-08-11 11:06:56 -0500

    icon moon

--- components/ui/icons/phosphor/IconMoon.tsx
+++ components/ui/icons/phosphor/IconMoon.tsx
@@ -1,7 +1,8 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
 import { SVGProps, memo } from "react";
 import { motion } from "framer-motion";
@@ -15,16 +16,16 @@ export const IconMoon = memo<
     }
 >(function IconMoon(
     /**
-     * Accessibility to customized attributes iconDirection, setMotion, and className
+     * @summary Accessibility to attributes iconDirection, setMotion, and className
      */
     { iconDirection = "0_rotation", setMotion, className },
     /**
-     * Accessibility to other SVG props
+     *@summary Accessibility to other SVG props
      */
-    props: SVGProps<SVGSVGElement>
+    { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
 ) {
     /**
-     * Create custom values for className
+     * @summary Creates custom values for className
      */
     const variants = cn(
         "block flex-grow-0 flex-shrink-0 w-[33.39px] h-[33.39px] relative",
@@ -39,7 +40,7 @@ export const IconMoon = memo<
         className
     );
     /**
-     * Establishes which variation of `IconMoon` to render
+     * @summary Establishes which variation of `IconMoon` to render
      */
     if (setMotion) {
         return (
@@ -54,9 +55,9 @@ export const IconMoon = memo<
             >
                 <motion.path
                     /**
-                     * Initial to animate, creates a fade-in-with-rotation animation
+                     * @summary From initial to animated, creates a fade-in-with-rotation animation
                      *
-                     * https://framer.com/motion/transition/##value-specific-transitions
+                     * @see https://framer.com/motion/transition/##value-specific-transitions
                      */
                     initial={{
                         rotate: 0,
@@ -73,7 +74,8 @@ export const IconMoon = memo<
                         duration: 0.34,
                         ease: "anticipate",
                     }}
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M236.37,139.4a12,12,0,0,0-12-3A84.07,84.07,0,0,1,119.6,31.59a12,12,0,0,0-15-15A108.86,108.86,0,0,0,49.69,55.07,108,108,0,0,0,136,228a107.09,107.09,0,0,0,64.93-21.69,108.86,108.86,0,0,0,38.44-54.94A12,12,0,0,0,236.37,139.4Zm-49.88,47.74A84,84,0,0,1,68.86,69.51,84.93,84.93,0,0,1,92.27,48.29Q92,52.13,92,56A108.12,108.12,0,0,0,200,164q3.87,0,7.71-.27A84.79,84.79,0,0,1,186.49,187.14Z"
                 />
             </motion.svg>
@@ -91,7 +93,8 @@ export const IconMoon = memo<
                 {...props}
             >
                 <path
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M236.37,139.4a12,12,0,0,0-12-3A84.07,84.07,0,0,1,119.6,31.59a12,12,0,0,0-15-15A108.86,108.86,0,0,0,49.69,55.07,108,108,0,0,0,136,228a107.09,107.09,0,0,0,64.93-21.69,108.86,108.86,0,0,0,38.44-54.94A12,12,0,0,0,236.37,139.4Zm-49.88,47.74A84,84,0,0,1,68.86,69.51,84.93,84.93,0,0,1,92.27,48.29Q92,52.13,92,56A108.12,108.12,0,0,0,200,164q3.87,0,7.71-.27A84.79,84.79,0,0,1,186.49,187.14Z"
                 ></path>
             </svg>

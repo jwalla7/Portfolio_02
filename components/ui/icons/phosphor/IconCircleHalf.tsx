a8810d7da5295fe6499c3ec50d3cfedb115c3bf2
jwalla7 <jwalla7@protonmail.com>
2023-08-11 11:24:07 -0500

    icon circle half

--- components/ui/icons/phosphor/IconCircleHalf.tsx
+++ components/ui/icons/phosphor/IconCircleHalf.tsx
@@ -1,8 +1,10 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
+
 import { SVGProps, memo } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
@@ -13,23 +15,23 @@ export const IconCircleHalf = memo<
     }
 >(function IconCircleHalf(
     /**
-     * Accessibility to customized attributes setMotion, and className
+     * @summary Accessibility to customized attributes setMotion, and className
      */
     { className, setMotion },
     /**
-     * Accessibility to other SVG props
+     * @summary Accessibility to other SVG props
      */
-    props: SVGProps<SVGSVGElement>
+    { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
 ) {
     /**
-     * Create custom values for className
+     * @summary Creates custom values for className
      */
     const variants = cn(
         "block flex-grow-0 flex-shrink-0 w-[36px] h-[36px] relative",
         className
     );
     /**
-     * Establishes which variation of `IconSun` to render
+     * @summary Establishes which variation of `IconSun` to render
      */
     if (setMotion) {
         return (
@@ -44,9 +46,9 @@ export const IconCircleHalf = memo<
             >
                 <motion.path
                     /**
-                     * Initial to animate, creates a fade-in-with-rotation animation
+                     * @summary From initial to animated, creates a fade-in-with-rotation animation
                      *
-                     * https://framer.com/motion/transition/##value-specific-transitions
+                     * @see https://framer.com/motion/transition/##value-specific-transitions
                      */
                     initial={{
                         opacity: 0,
@@ -61,7 +63,8 @@ export const IconCircleHalf = memo<
                         duration: 0.55,
                         ease: "anticipate",
                     }}
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24ZM40,128a88.1,88.1,0,0,1,88-88V216A88.1,88.1,0,0,1,40,128Z"
                 />
             </motion.svg>
@@ -79,7 +82,8 @@ export const IconCircleHalf = memo<
                 {...props}
             >
                 <path
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24ZM40,128a88.1,88.1,0,0,1,88-88V216A88.1,88.1,0,0,1,40,128Z"
                 ></path>
             </svg>

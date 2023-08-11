f124b7fe778f473b77e14fe365cbf087b212a09e
jwalla7 <jwalla7@protonmail.com>
2023-08-11 10:24:12 -0500

    icon x

--- components/ui/icons/phosphor/IconX.tsx
+++ components/ui/icons/phosphor/IconX.tsx
@@ -1,15 +1,17 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
+
 import { SVGProps, memo } from "react";
 
 export const IconX = memo<React.JSX.IntrinsicElements["svg"]>(function IconX(
     /**
-     * Accessibility to other SVG props
+     * @summary Accessibility to other SVG props
      */
-    props: SVGProps<SVGSVGElement>
+    { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
 ) {
     return (
         <svg
@@ -22,7 +24,8 @@ export const IconX = memo<React.JSX.IntrinsicElements["svg"]>(function IconX(
             {...props}
         >
             <path
-                fill="currentColor"
+                fill={fill}
+                fillOpacity={fillOpacity}
                 d="M208.49,191.51a12,12,0,0,1-17,17L128,145,64.49,208.49a12,12,0,0,1-17-17L111,128,47.51,64.49a12,12,0,0,1,17-17L128,111l63.51-63.52a12,12,0,0,1,17,17L145,128Z"
             ></path>
         </svg>

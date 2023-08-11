4743028d4dac431a50446c5dcc08201e55d31a91
jwalla7 <jwalla7@protonmail.com>
2023-08-11 11:15:44 -0500

    icon equalizer

--- components/ui/icons/phosphor/IconEqualizer.tsx
+++ components/ui/icons/phosphor/IconEqualizer.tsx
@@ -1,16 +1,18 @@
 /**
+ * @description
  * memo bypasses re-rendering a component if its props are unchanged.
  *
- * https://react.dev/reference/react/memo
+ * @see https://react.dev/reference/react/memo
  */
+
 import { SVGProps, memo } from "react";
 
 export const IconEqualizer = memo<React.ComponentProps<"svg">>(
     function IconEqualizer(
         /**
-         * Accessibility to other SVG props
+         * @summary Accessibility to other SVG props
          */
-        props: SVGProps<SVGSVGElement>
+        { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
     ) {
         return (
             <svg
@@ -23,7 +25,8 @@ export const IconEqualizer = memo<React.ComponentProps<"svg">>(
                 {...props}
             >
                 <path
-                    fill="currentColor"
+                    fill={fill}
+                    fillOpacity={fillOpacity}
                     d="M80,108a12,12,0,0,1-12,12H24a12,12,0,0,1,0-24H68A12,12,0,0,1,80,108ZM68,136H24a12,12,0,0,0,0,24H68a12,12,0,0,0,0-24Zm0,40H24a12,12,0,0,0,0,24H68a12,12,0,0,0,0-24Zm82-40H106a12,12,0,0,0,0,24h44a12,12,0,0,0,0-24Zm0,40H106a12,12,0,0,0,0,24h44a12,12,0,0,0,0-24Zm38-96h44a12,12,0,0,0,0-24H188a12,12,0,0,0,0,24Zm44,16H188a12,12,0,0,0,0,24h44a12,12,0,0,0,0-24Zm0,40H188a12,12,0,0,0,0,24h44a12,12,0,0,0,0-24Zm0,40H188a12,12,0,0,0,0,24h44a12,12,0,0,0,0-24Z"
                 ></path>
             </svg>

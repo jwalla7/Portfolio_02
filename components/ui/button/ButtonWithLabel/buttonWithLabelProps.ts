c2af25276d59f5002d5b1ac9ce7cf8588c6a8a47
jwalla7 <jwalla7@protonmail.com>
2023-08-12 17:43:57 -0500

    button with label prop types

--- components/ui/button/ButtonWithLabel/buttonWithLabelProps.ts
+++ components/ui/button/ButtonWithLabel/buttonWithLabelProps.ts
@@ -1,8 +1,34 @@
+/**
+ * @description
+ * A shape that inherits the rootDiv, labelDiv, text, and button types from Type_ButtonWithLabel.
+ *
+ * @see https://cva.style/docs/getting-started/typescript
+ */
+
 import { Type_ButtonWithLabel } from "@/types/button-with-label";
 export interface ButtonWithLabelProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+        /**
+         * Omit
+         *
+         * Omits the keys of the rootDiv, labelDiv, text, and button types from
+         *
+         * the Type_ButtonWithLabel type declaration.
+         */
         Omit<Type_ButtonWithLabel, "rootDiv" | "labelDiv" | "text" | "button">,
+        /**
+         * Required
+         *
+         * Makes all properties required in the Pick type declaration.
+         */
         Required<
+            /**
+             * Pick
+             *
+             * Enables inheritance of the rootDiv, labelDiv, text, and button types from
+             *
+             * the Type_ButtonWithLabel type declaration.
+             */
             Pick<
                 Type_ButtonWithLabel,
                 "rootDiv" | "labelDiv" | "text" | "button"

00f3498fe69c42cb58c58628977ca309031e7689
jwalla7 <jwalla7@protonmail.com>
2023-08-12 10:28:55 -0500

    icon file

--- components/ui/icons/phosphor/IconFile.tsx
+++ components/ui/icons/phosphor/IconFile.tsx
@@ -9,25 +9,27 @@ import React, { SVGProps, memo } from "react";
 
 export const IconFile = memo<React.ComponentProps<"svg">>(function IconFile(
     /**
-     * @summary Accessibility to other SVG props
+     * Accessibility to other SVG props
      */
-    { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
+    { fill = "currentColor", fillOpacity, ...props }: SVGProps<SVGSVGElement>
 ) {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fab"
-            data-icon="phosphorIcon-file"
-            viewBox="0 0 256 256"
-            {...props}
-        >
-            <path
-                fill={fill}
-                fillOpacity={fillOpacity}
-                d="M216.49,79.52l-56-56A12,12,0,0,0,152,20H56A20,20,0,0,0,36,40V216a20,20,0,0,0,20,20H200a20,20,0,0,0,20-20V88A12,12,0,0,0,216.49,79.52ZM160,57l23,23H160ZM60,212V44h76V92a12,12,0,0,0,12,12h48V212Z"
-            ></path>
-        </svg>
+        <i>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="phosphorIcon-file"
+                viewBox="0 0 256 256"
+                {...props}
+            >
+                <path
+                    fill={fill}
+                    fillOpacity={fillOpacity}
+                    d="M216.49,79.52l-56-56A12,12,0,0,0,152,20H56A20,20,0,0,0,36,40V216a20,20,0,0,0,20,20H200a20,20,0,0,0,20-20V88A12,12,0,0,0,216.49,79.52ZM160,57l23,23H160ZM60,212V44h76V92a12,12,0,0,0,12,12h48V212Z"
+                ></path>
+            </svg>
+        </i>
     );
 });

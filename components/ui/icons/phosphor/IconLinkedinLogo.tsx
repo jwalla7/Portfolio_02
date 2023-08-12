377944613d1c0ade9d2d95ea005fafe92fdcfeb9
jwalla7 <jwalla7@protonmail.com>
2023-08-12 10:26:31 -0500

    icon linkedin logo

--- components/ui/icons/phosphor/IconLinkedinLogo.tsx
+++ components/ui/icons/phosphor/IconLinkedinLogo.tsx
@@ -10,26 +10,32 @@ import { SVGProps, memo } from "react";
 export const IconLinkedinLogo = memo<React.ComponentProps<"svg">>(
     function IconLinkedinLogo(
         /**
-         * @summary Accessibility to other SVG props
+         * Accessibility to other SVG props
          */
-        { fill, fillOpacity, ...props }: SVGProps<SVGSVGElement>
+        {
+            fill = "currentColor",
+            fillOpacity,
+            ...props
+        }: SVGProps<SVGSVGElement>
     ) {
         return (
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="phosphorIcon-linkedinLogo"
-                viewBox="0 0 256 256"
-                {...props}
-            >
-                <path
-                    fill={fill}
-                    fillOpacity={fillOpacity}
-                    d="M216,20H40A20,20,0,0,0,20,40V216a20,20,0,0,0,20,20H216a20,20,0,0,0,20-20V40A20,20,0,0,0,216,20Zm-4,192H44V44H212ZM112,176V124a12,12,0,0,1,21.43-7.41A40,40,0,0,1,192,152v24a12,12,0,0,1-24,0V152a16,16,0,0,0-32,0v24a12,12,0,0,1-24,0ZM96,124v52a12,12,0,0,1-24,0V124a12,12,0,0,1,24,0ZM68,80A16,16,0,1,1,84,96,16,16,0,0,1,68,80Z"
-                ></path>
-            </svg>
+            <i>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="phosphorIcon-linkedinLogo"
+                    viewBox="0 0 256 256"
+                    {...props}
+                >
+                    <path
+                        fill={fill}
+                        fillOpacity={fillOpacity}
+                        d="M216,20H40A20,20,0,0,0,20,40V216a20,20,0,0,0,20,20H216a20,20,0,0,0,20-20V40A20,20,0,0,0,216,20Zm-4,192H44V44H212ZM112,176V124a12,12,0,0,1,21.43-7.41A40,40,0,0,1,192,152v24a12,12,0,0,1-24,0V152a16,16,0,0,0-32,0v24a12,12,0,0,1-24,0ZM96,124v52a12,12,0,0,1-24,0V124a12,12,0,0,1,24,0ZM68,80A16,16,0,1,1,84,96,16,16,0,0,1,68,80Z"
+                    ></path>
+                </svg>
+            </i>
         );
     }
 );

c6864e9269f392b4ba67f960548fd2e35bf692cf
jwalla7 <jwalla7@protonmail.com>
2023-08-12 10:22:10 -0500

    icon x

--- components/ui/icons/phosphor/IconX.tsx
+++ components/ui/icons/phosphor/IconX.tsx
@@ -14,20 +14,22 @@ export const IconX = memo<React.JSX.IntrinsicElements["svg"]>(function IconX(
     { fill = "currentColor", fillOpacity, ...props }: SVGProps<SVGSVGElement>
 ) {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fab"
-            data-icon="phosphorIcon-X"
-            viewBox="0 0 256 256"
-            {...props}
-        >
-            <path
-                fill={fill}
-                fillOpacity={fillOpacity}
-                d="M208.49,191.51a12,12,0,0,1-17,17L128,145,64.49,208.49a12,12,0,0,1-17-17L111,128,47.51,64.49a12,12,0,0,1,17-17L128,111l63.51-63.52a12,12,0,0,1,17,17L145,128Z"
-            ></path>
-        </svg>
+        <i>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="phosphorIcon-X"
+                viewBox="0 0 256 256"
+                {...props}
+            >
+                <path
+                    fill={fill}
+                    fillOpacity={fillOpacity}
+                    d="M208.49,191.51a12,12,0,0,1-17,17L128,145,64.49,208.49a12,12,0,0,1-17-17L111,128,47.51,64.49a12,12,0,0,1,17-17L128,111l63.51-63.52a12,12,0,0,1,17,17L145,128Z"
+                ></path>
+            </svg>
+        </i>
     );
 });

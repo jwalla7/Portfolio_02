9f7bc2315189982fbd054d9b0b652a09ec44fe74
jwalla7 <jwalla7@protonmail.com>
2023-08-14 16:59:28 -0500

    icon circle half

--- components/ui/icons/phosphor/IconCircleHalf.tsx
+++ components/ui/icons/phosphor/IconCircleHalf.tsx
@@ -35,39 +35,41 @@ export const IconCircleHalf = memo<
      */
     if (setMotion) {
         return (
-            <motion.svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="phosphorIcon-circle-half"
-                viewBox="0 0 256 256"
-                className={variants}
-            >
-                <motion.path
-                    /**
-                     * @summary From initial to animated, creates a fade-in-with-rotation animation
-                     *
-                     * @see https://framer.com/motion/transition/##value-specific-transitions
-                     */
-                    initial={{
-                        opacity: 0,
-                        pathLength: 0,
-                    }}
-                    animate={{
-                        opacity: 1,
-                        pathLength: 1,
-                    }}
-                    transition={{
-                        delay: 0.08,
-                        duration: 0.55,
-                        ease: "anticipate",
-                    }}
-                    fill={fill}
-                    fillOpacity={fillOpacity}
-                    d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24ZM40,128a88.1,88.1,0,0,1,88-88V216A88.1,88.1,0,0,1,40,128Z"
-                />
-            </motion.svg>
+            <i>
+                <motion.svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="phosphorIcon-circle-half"
+                    viewBox="0 0 256 256"
+                    className={variants}
+                >
+                    <motion.path
+                        /**
+                         * @summary From initial to animated, creates a fade-in-with-rotation animation
+                         *
+                         * @see https://framer.com/motion/transition/##value-specific-transitions
+                         */
+                        initial={{
+                            opacity: 0,
+                            pathLength: 0,
+                        }}
+                        animate={{
+                            opacity: 1,
+                            pathLength: 1,
+                        }}
+                        transition={{
+                            delay: 0.08,
+                            duration: 0.55,
+                            ease: "anticipate",
+                        }}
+                        fill={fill}
+                        fillOpacity={fillOpacity}
+                        d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24ZM40,128a88.1,88.1,0,0,1,88-88V216A88.1,88.1,0,0,1,40,128Z"
+                    />
+                </motion.svg>
+            </i>
         );
     } else {
         return (

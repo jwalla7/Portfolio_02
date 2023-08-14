ade21c55c8d0b66b8b0d79eb6e4582c1eee2b8ce
jwalla7 <jwalla7@protonmail.com>
2023-08-14 17:05:48 -0500

    icon sun

--- components/ui/icons/phosphor/IconSun.tsx
+++ components/ui/icons/phosphor/IconSun.tsx
@@ -35,42 +35,44 @@ export const IconSun = memo<
      */
     if (setMotion) {
         return (
-            <motion.svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="phosphorIcon-sun"
-                viewBox="0 0 256 256"
-                preserveAspectRatio="xMidYMid meet"
-                className={variants}
-            >
-                <motion.path
-                    /**
-                     * @summary From initial to animated, creates a fade-in-with-rotation animation
-                     *
-                     * @see https://framer.com/motion/transition/##value-specific-transitions
-                     */
-                    initial={{
-                        rotate: 0,
-                        opacity: 0,
-                        pathLength: 0,
-                    }}
-                    animate={{
-                        rotate: 45,
-                        opacity: 1,
-                        pathLength: 1,
-                    }}
-                    transition={{
-                        delay: 0.08,
-                        duration: 0.55,
-                        ease: "anticipate",
-                    }}
-                    fill={fill}
-                    fillOpacity={fillOpacity}
-                    d="M116,32V16a12,12,0,0,1,24,0V32a12,12,0,0,1-24,0Zm80,96a68,68,0,1,1-68-68A68.07,68.07,0,0,1,196,128Zm-24,0a44,44,0,1,0-44,44A44.05,44.05,0,0,0,172,128ZM51.51,68.49a12,12,0,1,0,17-17l-12-12a12,12,0,0,0-17,17Zm0,119-12,12a12,12,0,0,0,17,17l12-12a12,12,0,1,0-17-17ZM196,72a12,12,0,0,0,8.49-3.51l12-12a12,12,0,0,0-17-17l-12,12A12,12,0,0,0,196,72Zm8.49,115.51a12,12,0,0,0-17,17l12,12a12,12,0,0,0,17-17ZM44,128a12,12,0,0,0-12-12H16a12,12,0,0,0,0,24H32A12,12,0,0,0,44,128Zm84,84a12,12,0,0,0-12,12v16a12,12,0,0,0,24,0V224A12,12,0,0,0,128,212Zm112-96H224a12,12,0,0,0,0,24h16a12,12,0,0,0,0-24Z"
-                />
-            </motion.svg>
+            <i>
+                <motion.svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="phosphorIcon-sun"
+                    viewBox="0 0 256 256"
+                    preserveAspectRatio="xMidYMid meet"
+                    className={variants}
+                >
+                    <motion.path
+                        /**
+                         * @summary From initial to animated, creates a fade-in-with-rotation animation
+                         *
+                         * @see https://framer.com/motion/transition/##value-specific-transitions
+                         */
+                        initial={{
+                            rotate: 0,
+                            opacity: 0,
+                            pathLength: 0,
+                        }}
+                        animate={{
+                            rotate: 45,
+                            opacity: 1,
+                            pathLength: 1,
+                        }}
+                        transition={{
+                            delay: 0.08,
+                            duration: 0.55,
+                            ease: "anticipate",
+                        }}
+                        fill={fill}
+                        fillOpacity={fillOpacity}
+                        d="M116,32V16a12,12,0,0,1,24,0V32a12,12,0,0,1-24,0Zm80,96a68,68,0,1,1-68-68A68.07,68.07,0,0,1,196,128Zm-24,0a44,44,0,1,0-44,44A44.05,44.05,0,0,0,172,128ZM51.51,68.49a12,12,0,1,0,17-17l-12-12a12,12,0,0,0-17,17Zm0,119-12,12a12,12,0,0,0,17,17l12-12a12,12,0,1,0-17-17ZM196,72a12,12,0,0,0,8.49-3.51l12-12a12,12,0,0,0-17-17l-12,12A12,12,0,0,0,196,72Zm8.49,115.51a12,12,0,0,0-17,17l12,12a12,12,0,0,0,17-17ZM44,128a12,12,0,0,0-12-12H16a12,12,0,0,0,0,24H32A12,12,0,0,0,44,128Zm84,84a12,12,0,0,0-12,12v16a12,12,0,0,0,24,0V224A12,12,0,0,0,128,212Zm112-96H224a12,12,0,0,0,0,24h16a12,12,0,0,0,0-24Z"
+                    />
+                </motion.svg>
+            </i>
         );
     } else {
         return (

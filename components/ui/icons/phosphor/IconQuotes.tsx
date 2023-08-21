6d8e316cf9cde6a5d904a011f7848c4ba576cbf1
jwalla7 <jwalla7@protonmail.com>
2023-08-21 14:23:56 -0500

    icon quotes

--- components/ui/icons/phosphor/IconQuotes.tsx
+++ components/ui/icons/phosphor/IconQuotes.tsx
@@ -27,7 +27,7 @@ export const IconQuotes = memo<React.ComponentProps<"svg">>(function IconQuotes(
         className
     );
     return (
-        <i>
+        <i className="place-self-stretch">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true"

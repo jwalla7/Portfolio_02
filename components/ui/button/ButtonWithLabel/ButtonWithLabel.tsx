e0212278cc4776c5b01d1c15c06eee0c9c56710e
jwalla7 <jwalla7@protonmail.com>
2023-08-23 10:25:17 -0500

    update

--- components/ui/button/ButtonWithLabel/ButtonWithLabel.tsx
+++ components/ui/button/ButtonWithLabel/ButtonWithLabel.tsx
@@ -14,13 +14,13 @@ export const ButtonWithLabel = ({
                 className="
                 bg-transparent
                 group-hover:bg-gradient-to-r from-[rgba(255,255,255,0.089)] from-0% via-[rgba(242,242,242,0.13)] from-76.04% to-neutral-50/[0.1]
-                box-border border-0 grow-0 shrink-0 justify-center items-center w-[3.9121265377855887vw] h-[6.9780564263323vh] relative gap-[13px] p-[6.89%] rounded-[12.4px] backdrop-blur-[12.4px]"
+                box-border border-0 grow-0 shrink-0 justify-center items-center w-[3.9121265377855887vw] h-[6.9780564263323vh] relative rounded-[12.4px] backdrop-blur-[12.4px]"
                 {...props}
                 ref={navButtonRef}
             >
                 {icon}
             </button>
-            <div className="bg-transparent hover:bg-transparent flex justify-start items-center relative place-self-center grow-0 shrink-0 gap-2.5 px-2.5 pt-[3px]">
+            <div className="bg-transparent hover:bg-transparent flex justify-start items-center relative place-self-center grow-0 shrink-0 gap-2.5 pt-[3px]">
                 <span
                     className="
                 group-hover:text-neutral-50

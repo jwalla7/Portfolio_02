770b2d42c6ed57e4053c8c4257329bd6ea886434
jwalla7 <jwalla7@protonmail.com>
2023-08-24 11:50:47 -0500

    icon file

--- components/ui/icons/phosphor/IconFile.tsx
+++ components/ui/icons/phosphor/IconFile.tsx
@@ -27,23 +27,21 @@ export const IconFile = memo<React.ComponentProps<"svg">>(function IconFile(
         className
     );
     return (
-        <i className="place-items-center place-self-stretch justify-self-center self-center">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="phosphorIcon-file"
-                viewBox="0 0 256 256"
-                className={clsx(variants, props.className)}
-                {...props}
-            >
-                <path
-                    fill={fill}
-                    fillOpacity={fillOpacity}
-                    d="M216.49,79.52l-56-56A12,12,0,0,0,152,20H56A20,20,0,0,0,36,40V216a20,20,0,0,0,20,20H200a20,20,0,0,0,20-20V88A12,12,0,0,0,216.49,79.52ZM160,57l23,23H160ZM60,212V44h76V92a12,12,0,0,0,12,12h48V212Z"
-                ></path>
-            </svg>
-        </i>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fab"
+            data-icon="phosphorIcon-file"
+            viewBox="0 0 256 256"
+            className={clsx(variants, props.className)}
+            {...props}
+        >
+            <path
+                fill={fill}
+                fillOpacity={fillOpacity}
+                d="M216.49,79.52l-56-56A12,12,0,0,0,152,20H56A20,20,0,0,0,36,40V216a20,20,0,0,0,20,20H200a20,20,0,0,0,20-20V88A12,12,0,0,0,216.49,79.52ZM160,57l23,23H160ZM60,212V44h76V92a12,12,0,0,0,12,12h48V212Z"
+            ></path>
+        </svg>
     );
 });

1dfddb216a5f645a1da7744795e71aee4d9bc750
jwalla7 <jwalla7@protonmail.com>
2023-08-24 11:51:38 -0500

    icon arrow circle left

--- components/ui/icons/phosphor/IconArrowCircleLeft.tsx
+++ components/ui/icons/phosphor/IconArrowCircleLeft.tsx
@@ -38,23 +38,21 @@ export const IconArrowCircleLeft = memo<
     );
 
     return (
-        <i className="place-self-stretch">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="phosphorIcon-arrowCircleLeft"
-                viewBox="0 0 256 256"
-                className={clsx(variants, props.className)}
-                {...props}
-            >
-                <path
-                    fill={fill}
-                    fillOpacity={fillOpacity}
-                    d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a8,8,0,0,1-8,8H107.31l18.35,18.34a8,8,0,0,1-11.32,11.32l-32-32a8,8,0,0,1,0-11.32l32-32a8,8,0,0,1,11.32,11.32L107.31,120H168A8,8,0,0,1,176,128Z"
-                ></path>
-            </svg>
-        </i>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fab"
+            data-icon="phosphorIcon-arrowCircleLeft"
+            viewBox="0 0 256 256"
+            className={clsx(variants, props.className)}
+            {...props}
+        >
+            <path
+                fill={fill}
+                fillOpacity={fillOpacity}
+                d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a8,8,0,0,1-8,8H107.31l18.35,18.34a8,8,0,0,1-11.32,11.32l-32-32a8,8,0,0,1,0-11.32l32-32a8,8,0,0,1,11.32,11.32L107.31,120H168A8,8,0,0,1,176,128Z"
+            ></path>
+        </svg>
     );
 });

bb3cec7ce1a23719e063d5d70a39e702c20b7b2b
jwalla7 <jwalla7@protonmail.com>
2023-08-24 11:52:39 -0500

    icon envelope simple

--- components/ui/icons/phosphor/IconEnvelopeSimple.tsx
+++ components/ui/icons/phosphor/IconEnvelopeSimple.tsx
@@ -32,24 +32,22 @@ export const IconEnvelopSimple = memo<React.ComponentProps<"svg">>(
             className
         );
         return (
-            <i className="place-self-stretch">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fab"
-                    data-icon="phosphorIcon-envelopeSimple"
-                    viewBox="0 0 256 256"
-                    className={clsx(variants, props.className)}
-                    {...props}
-                >
-                    <path
-                        fill={fill}
-                        fillOpacity={fillOpacity}
-                        d="M224,44H32A12,12,0,0,0,20,56V192a20,20,0,0,0,20,20H216a20,20,0,0,0,20-20V56A12,12,0,0,0,224,44ZM193.15,68,128,127.72,62.85,68ZM44,188V83.28l75.89,69.57a12,12,0,0,0,16.22,0L212,83.28V188Z"
-                    ></path>
-                </svg>
-            </i>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="phosphorIcon-envelopeSimple"
+                viewBox="0 0 256 256"
+                className={clsx(variants, props.className)}
+                {...props}
+            >
+                <path
+                    fill={fill}
+                    fillOpacity={fillOpacity}
+                    d="M224,44H32A12,12,0,0,0,20,56V192a20,20,0,0,0,20,20H216a20,20,0,0,0,20-20V56A12,12,0,0,0,224,44ZM193.15,68,128,127.72,62.85,68ZM44,188V83.28l75.89,69.57a12,12,0,0,0,16.22,0L212,83.28V188Z"
+                ></path>
+            </svg>
         );
     }
 );

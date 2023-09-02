f4b56309b9ef733919f58ce16477c8b409719351
jwalla7 <jwalla7@protonmail.com>
2023-09-02 14:09:27 -0500

    update

--- components/ui/button/ButtonWithLabel/ButtonWithLabel.tsx
+++ components/ui/button/ButtonWithLabel/ButtonWithLabel.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import clsx from "clsx";
 import { ButtonWithLabelProps } from "./buttonWithLabelProps";
 import { robotoRegular } from "@/design/fontDefaults";
 import { LegacyRef, forwardRef } from "react";
+import { cn } from "@/lib/utils";
 
 export const ButtonWithLabel = forwardRef(
     (
@@ -13,10 +13,10 @@ export const ButtonWithLabel = forwardRef(
         return (
             <div className="flex justify-start items-center gap-x-[1.23vw] gap-y-[2.194vh] group">
                 <button
-                    className={clsx(
+                    className={cn(
                         "box-border border-0 grow-0 shrink-0 justify-center items-center w-[3.9121265377855887vw] h-[6.9780564263323vh] relative rounded-[12.4px] backdrop-blur-[12.4px] px-[9%]",
                         active
-                            ? "bg-[#E6E6E6] group-hover:bg-secondary/80"
+                            ? "group-hover:bg-[#E6E6E6] bg-secondary/80 dark:group-hover:bg-[#E6E6E6] dark:bg-[hsl(240,4.8%,95.9%)]"
                             : "bg-transparent group-hover:bg-gradient-to-r from-[rgba(255,255,255,0.089)] from-0% via-[rgba(242,242,242,0.13)] from-76.04% to-neutral-50/[0.1]"
                     )}
                     {...props}
@@ -28,7 +28,7 @@ export const ButtonWithLabel = forwardRef(
                 </button>
                 <div className="bg-transparent hover:bg-transparent flex justify-start items-center relative place-items-center grow-0 shrink-0 gap-2.5 pt-[3px]">
                     <span
-                        className={clsx(
+                        className={cn(
                             "group-hover:text-neutral-50 text-neutral-50/[0.34] whitespace-pre-line grow-0 shrink-0 text-[1.625rem] leading-normal text-left hover:cursor-auto",
                             robotoRegular.className
                         )}

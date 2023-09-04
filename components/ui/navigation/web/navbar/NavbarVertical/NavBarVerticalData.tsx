ec05b4d3f82f2028486648714291075b18c78b2d
jwalla7 <jwalla7@protonmail.com>
2023-09-04 16:00:40 -0500

    update

--- components/ui/navigation/web/navbar/NavbarVertical/NavBarVerticalData.tsx
+++ components/ui/navigation/web/navbar/NavbarVertical/NavBarVerticalData.tsx
@@ -1,5 +1,12 @@
 "use client";
-
+/**
+ * @description
+ * Generates and manages data for navigation buttons.
+ *
+ * It uses a Map data structure to store button properties like labels, icons, and routes, dynamically changing them based on the current URL.
+ *
+ * Data is memoized, making it available for rendering navigation buttons in components based on the URL.
+ */
 import { useMemo, useRef } from "react";
 import { ButtonWithLabelProps } from "@/components/ui/button/ButtonWithLabel/buttonWithLabelProps";
 import { IconFile } from "@/components/ui/icons/phosphor/IconFile";
@@ -8,14 +15,35 @@ import { IconEnvelopSimple } from "@/components/ui/icons/phosphor/IconEnvelopeSi
 import { IconHouse } from "@/components/ui/icons/phosphor/IconHouse";
 import { IconQuotes } from "@/components/ui/icons/phosphor/IconQuotes";
 import { usePathname } from "next/dist/client/components/navigation";
+import { IconSun } from "@/components/ui/icons/phosphor/IconSun";
 
 export function useNavButtonData() {
     const navButtonRef = useRef<HTMLButtonElement | null>(null);
+    /**
+     * usePathname
+     *
+     * Returns the current URL.
+     *
+     * @see https://nextjs.org/docs/api-reference/next/router#useroutepathname
+     */
     const url = usePathname();
-
+    /**
+     * useMemo
+     *
+     * Recalculates data when the URL changes.
+     *
+     * @see https://react.dev/reference/react/useMemo
+     */
     const navButtonData = useMemo(() => {
+        /**
+         * Map
+         *
+         * JavaScript built-in Map data structure, which stores key-value pairs in order of insertion.
+         */
         const navMap = new Map<string, ButtonWithLabelProps>();
-
+        /**
+         * Home Navigation Button Data
+         */
         navMap.set("Home", {
             rootdiv: "default",
             buttondiv: "default",
@@ -36,6 +64,9 @@ export function useNavButtonData() {
             route: "/main",
             buttoneventsref: navButtonRef,
         });
+        /**
+         * Resume Navigation Button Data
+         */
         navMap.set("Resume", {
             rootdiv: "default",
             buttondiv: "default",
@@ -56,6 +87,9 @@ export function useNavButtonData() {
             route: "/resume",
             buttoneventsref: navButtonRef,
         });
+        /**
+         * Music Navigation Button Data
+         */
         navMap.set("Music", {
             rootdiv: "default",
             buttondiv: "default",
@@ -76,6 +110,9 @@ export function useNavButtonData() {
             route: "/music",
             buttoneventsref: navButtonRef,
         });
+        /**
+         * Email Navigation Button Data
+         */
         navMap.set("Email", {
             rootdiv: "default",
             buttondiv: "default",
@@ -96,6 +133,9 @@ export function useNavButtonData() {
             route: "/contact",
             buttoneventsref: navButtonRef,
         });
+        /**
+         * Daily Quotes Navigation Button Data
+         */
         navMap.set("Daily_Quotes", {
             rootdiv: "default",
             buttondiv: "default",
@@ -116,7 +156,6 @@ export function useNavButtonData() {
             route: "/messages",
             buttoneventsref: navButtonRef,
         });
-
         return navMap;
     }, [url]);
 
@@ -127,17 +166,7 @@ export function useThemeButtonData() {
     const themeButtonRef = useRef(null);
 
     const themeButtonData = useMemo(() => {
-        const themeMap = new Map<string, ButtonWithLabelProps>();
-        themeMap.set("Home", {
-            rootdiv: "default",
-            buttondiv: "default",
-            labeldiv: "default",
-            textdiv: "default",
-            label: "Home",
-            icon: <IconHouse fillOpacity={100} active={true} className="" />,
-            route: "/main",
-            buttoneventsref: themeButtonRef,
-        });
+        const themeMap = new Map<string, HTMLButtonElement>();
         return themeMap;
     }, []);
 

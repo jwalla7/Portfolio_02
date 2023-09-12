ca08386d845ea140805a8c9f86edc5ab7796d30e
jwalla7 <jwalla7@protonmail.com>
2023-09-11 21:34:38 -0500

    update

--- components/ui/button/ButtonWithLabel/ButtonWithLabel.tsx
+++ components/ui/button/ButtonWithLabel/ButtonWithLabel.tsx
@@ -5,99 +5,62 @@ import { ButtonWithLabelProps } from "./buttonWithLabelProps";
 import { btnWithLblButtonStyles } from "./buttonWithLabelStyles";
 import { cn } from "@/lib/utils";
 import { robotoRegular } from "@/design/fontDefaults";
-
-export const ButtonWithLabel =
-    forwardRef<
-        HTMLButtonElement,
-        ButtonWithLabelProps
-    >(
-        (
-            {
-                icon,
-                label,
-                active,
-                ...props
-            },
-            buttoneventsref
-        ) => {
-            return (
-                <div
+//
+export const ButtonWithLabel = forwardRef<HTMLButtonElement, ButtonWithLabelProps>(({ icon, label, active, ...props }, buttoneventsref) => {
+    return (
+        <div
+            className={cn(
+                btnWithLblButtonStyles({
+                    buttonDiv: "default",
+                })
+            )}
+        >
+            <button
+                className={cn(
+                    btnWithLblButtonStyles({
+                        button: "default",
+                    }),
+                    active
+                        ? btnWithLblButtonStyles({
+                              button: "active",
+                          })
+                        : btnWithLblButtonStyles({
+                              button: "inactive",
+                          })
+                )}
+                {...props}
+                ref={buttoneventsref}
+            >
+                <i
                     className={cn(
-                        btnWithLblButtonStyles(
-                            {
-                                buttonDiv:
-                                    "default",
-                            }
-                        )
+                        btnWithLblButtonStyles({
+                            iconDiv: "default",
+                        })
                     )}
                 >
-                    <button
-                        className={cn(
-                            btnWithLblButtonStyles(
-                                {
-                                    button: "default",
-                                }
-                            ),
-                            active
-                                ? btnWithLblButtonStyles(
-                                      {
-                                          button: "active",
-                                      }
-                                  )
-                                : btnWithLblButtonStyles(
-                                      {
-                                          button: "inactive",
-                                      }
-                                  )
-                        )}
-                        {...props}
-                        ref={
-                            buttoneventsref
-                        }
-                    >
-                        <i
-                            className={cn(
-                                btnWithLblButtonStyles(
-                                    {
-                                        iconDiv:
-                                            "default",
-                                    }
-                                )
-                            )}
-                        >
-                            {icon}
-                        </i>
-                    </button>
-                    <div
-                        className={cn(
-                            btnWithLblButtonStyles(
-                                {
-                                    labelDiv:
-                                        "default",
-                                }
-                            )
-                        )}
-                    >
-                        <label
-                            className={cn(
-                                btnWithLblButtonStyles(
-                                    {
-                                        labelText:
-                                            "default",
-                                    }
-                                ),
-                                robotoRegular.className
-                            )}
-                        >
-                            {
-                                label
-                            }
-                        </label>
-                    </div>
-                </div>
-            );
-        }
+                    {icon}
+                </i>
+            </button>
+            <div
+                className={cn(
+                    btnWithLblButtonStyles({
+                        labelDiv: "default",
+                    })
+                )}
+            >
+                <label
+                    className={cn(
+                        btnWithLblButtonStyles({
+                            labelText: "default",
+                        }),
+                        robotoRegular.className
+                    )}
+                >
+                    {label}
+                </label>
+            </div>
+        </div>
     );
+});
 
-ButtonWithLabel.displayName =
-    "ButtonWithLabel";
+ButtonWithLabel.displayName = "ButtonWithLabel";

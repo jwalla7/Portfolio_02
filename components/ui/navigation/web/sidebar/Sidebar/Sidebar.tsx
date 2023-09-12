47db11ee814b0726d8807cb5b052e4c554fe1fce
jwalla7 <jwalla7@protonmail.com>
2023-09-12 11:22:02 -0500

    update

--- components/ui/navigation/web/sidebar/Sidebar/Sidebar.tsx
+++ components/ui/navigation/web/sidebar/Sidebar/Sidebar.tsx
@@ -2,20 +2,20 @@
 
 /**
  * @description
- * This component creates a vertical navigation bar with nested nav and theme buttons.
+ * This component creates a vertical navigation bar with nested nav and theme buttons
  */
 
 import { forwardRef, useTransition } from "react";
 import { ButtonWithLabel } from "@/components/ui/button/ButtonWithLabel/ButtonWithLabel";
-import { SidebarProps } from "./sidebarProps";
 import { NavigationMenuLink } from "@radix-ui/react-navigation-menu";
-import { cn } from "@/lib/utils";
+import { SidebarProps } from "./sidebarProps";
 import { ThemeToggleGroup } from "@/components/ui/theme/toggle/ThemeToggleGroup/ThemeToggleGroup";
-import { usePathname } from "next/dist/client/components/navigation";
-import { sidebarStyles } from "./sidebarStyles";
 import { ToggleGroup } from "@radix-ui/react-toggle-group";
+import { cn } from "@/lib/utils";
+import { sidebarStyles } from "./sidebarStyles";
 import { themeToggleGroupStyles } from "@/components/ui/theme/toggle/ThemeToggleGroup/themeToggleGroupStyles";
 import { useNavButtonData } from "@/components/hooks/useNavButtonData/useNavButtonData";
+import { usePathname } from "next/dist/client/components/navigation";
 import { useThemeButtonData } from "@/components/hooks/useThemeButtonData/useThemeButtonData";
 
 export const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(({ overlayRefProps }, ref) => {
@@ -44,16 +44,47 @@ export const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(({ overlayRefPro
                                 }
                             }}
                         >
-                            <ButtonWithLabel key={index} rootdiv={buttonID?.rootdiv} buttondiv={buttonID?.buttondiv} labeldiv={buttonID?.labeldiv} textdiv={buttonID?.textdiv} label={buttonID?.label} icon={buttonID?.icon} route={buttonID?.route} buttoneventsref={buttonID?.buttoneventsref} active={buttonID?.route === url} />
+                            <ButtonWithLabel
+                                key={index}
+                                rootdiv={buttonID?.rootdiv}
+                                buttondiv={buttonID?.buttondiv}
+                                labeldiv={buttonID?.labeldiv}
+                                textdiv={buttonID?.textdiv}
+                                label={buttonID?.label}
+                                icon={buttonID?.icon}
+                                route={buttonID?.route}
+                                buttoneventsref={buttonID?.buttoneventsref}
+                                active={buttonID?.route === url}
+                            />
                         </NavigationMenuLink>
                     );
                 })}
             </div>
             <div className={cn(sidebarStyles({ theme: "active" }))} ref={ref}>
-                <ToggleGroup className={cn(themeToggleGroupStyles({ togglediv: "default" }))} type="single" orientation="horizontal" aria-label="Text alignment">
+                <ToggleGroup
+                    className={cn(themeToggleGroupStyles({ togglediv: "default" }))}
+                    type="single"
+                    orientation="horizontal"
+                    aria-label="Text alignment"
+                >
                     {Array.from(themeButtonData.keys()).map((key, index) => {
                         const themeButtonID = themeButtonData.get(key);
-                        return <ThemeToggleGroup key={index} active title={themeButtonID?.title} togglediv={themeButtonID?.togglediv} itemdiv={themeButtonID?.itemdiv} labeldiv={themeButtonID?.labeldiv} labeltext={themeButtonID?.labeltext} icondiv={themeButtonID?.icondiv} label={themeButtonID?.label} icon={themeButtonID?.icon} value={themeButtonID?.value} buttoneventsref={themeButtonID?.buttoneventsref} />;
+                        return (
+                            <ThemeToggleGroup
+                                key={index}
+                                active
+                                title={themeButtonID?.title}
+                                togglediv={themeButtonID?.togglediv}
+                                itemdiv={themeButtonID?.itemdiv}
+                                labeldiv={themeButtonID?.labeldiv}
+                                labeltext={themeButtonID?.labeltext}
+                                icondiv={themeButtonID?.icondiv}
+                                label={themeButtonID?.label}
+                                icon={themeButtonID?.icon}
+                                value={themeButtonID?.value}
+                                buttoneventsref={themeButtonID?.buttoneventsref}
+                            />
+                        );
                     })}
                 </ToggleGroup>
             </div>

ac9bd603d1585b099c853b1d8d76cad2a309f760
jwalla7 <jwalla7@protonmail.com>
2023-09-11 21:38:41 -0500

    update

--- components/ui/button/ButtonWithLabel/ButtonWithLabel.tsx
+++ components/ui/button/ButtonWithLabel/ButtonWithLabel.tsx
@@ -5,11 +5,18 @@ import { ButtonWithLabelProps } from "./buttonWithLabelProps";
 import { btnWithLblButtonStyles } from "./buttonWithLabelStyles";
 import { cn } from "@/lib/utils";
 import { robotoRegular } from "@/design/fontDefaults";
-
+// s
 export const ButtonWithLabel = forwardRef<HTMLButtonElement, ButtonWithLabelProps>(({ icon, label, active, ...props }, buttoneventsref) => {
     return (
         <div className={cn(btnWithLblButtonStyles({ buttonDiv: "default" }))}>
-            <button className={cn(btnWithLblButtonStyles({ button: "default" }), active ? btnWithLblButtonStyles({ button: "active" }) : btnWithLblButtonStyles({ button: "inactive" }))} {...props} ref={buttoneventsref}>
+            <button
+                className={cn(
+                    btnWithLblButtonStyles({ button: "default" }),
+                    active ? btnWithLblButtonStyles({ button: "active" }) : btnWithLblButtonStyles({ button: "inactive" })
+                )}
+                {...props}
+                ref={buttoneventsref}
+            >
                 <i className={cn(btnWithLblButtonStyles({ iconDiv: "default" }))}>{icon}</i>
             </button>
             <div className={cn(btnWithLblButtonStyles({ labelDiv: "default" }))}>

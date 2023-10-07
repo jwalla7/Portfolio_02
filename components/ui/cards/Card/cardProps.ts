e21f3ce3556e251cf2ed70ae81c9c815a2e4549c
jwalla7 <jwalla7@protonmail.com>
2023-10-07 10:41:07 -0500

    update

--- components/ui/cards/Card/cardProps.ts
+++ components/ui/cards/Card/cardProps.ts
@@ -2,10 +2,7 @@ import { Type_CardPosition, Type_CardSize } from "@/types/card";
 import { ReactElement, ReactNode } from "react";
 
 export interface CardProps {
-    children?: ReactNode[];
-    // leftChildren?: ReactElement;
-    // centerChildren?: ReactElement;
-    // rightChildren?: ReactElement;
+    children?: ReactElement[];
     size: Type_CardSize;
     position: Type_CardPosition;
 }

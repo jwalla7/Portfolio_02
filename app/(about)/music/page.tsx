f040115c8a17a83bad5cf838d3f336de83cfe196
jwalla7 <jwalla7@protonmail.com>
2023-10-07 17:08:56 -0500

    update

--- app/(about)/music/page.tsx
+++ app/(about)/music/page.tsx
@@ -7,7 +7,7 @@ import { Card } from "@/components/ui/cards/Card/Card";
 
 export default function MusicPage() {
     return (
-        <div suppressHydrationWarning className="min-w-full min-h-screen h-screen overflow-hidden">
+        <div suppressHydrationWarning className="relative min-w-full min-h-screen h-screen overflow-hidden">
             <BackgroundImage imageLightThemeSrc="/images/CNTRS-lightmode.png" imageDarkThemeSrc="/images/CNTRS-darkmode.png" />
             <CardGroup>
                 <Card size="lg" position="center" />

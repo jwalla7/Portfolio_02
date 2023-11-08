4986231704a7e3b83c4c781be95c223a77f6871c
jwalla7 <jwalla7@protonmail.com>
2023-11-08 17:37:42 -0600

    update audius

---
+++ app/api/audius/route.ts
@@ -0,0 +1,32 @@
+import { sdk } from "@audius/sdk";
+import { env } from "@/env.mjs";
+
+export const audiusTest = async () => {
+    try {
+        const apiKey = env.AUDIUS_API_KEY;
+        const apiSecret = env.AUDIUS_SECRET;
+
+        // Ensure the SDK is initialized correctly
+        if (!apiKey || !apiSecret) {
+            throw new Error("API key or secret is missing");
+        }
+
+        const audiusSdk = sdk({
+            appName: "PortfolioV2",
+            apiKey: apiKey,
+            apiSecret: apiSecret,
+        });
+
+        const { data: track } = await audiusSdk.tracks.getTrack({
+            trackId: "1B5ab8z",
+        });
+        console.log("TRACK FETCH: ", track);
+
+        const { data: trackResolve } = await audiusSdk.resolve({
+            url: "https://audius.co/jwalla7/test-smpl",
+        });
+        console.log("TRACK RESOLVE: ", trackResolve);
+    } catch (error) {
+        console.error("Error in audiusTest: ", error);
+    }
+};

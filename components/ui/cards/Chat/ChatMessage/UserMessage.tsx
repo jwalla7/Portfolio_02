ad7717fd06e7d73c999d6e2763104fd149c22dfe
jwalla7 <jwalla7@protonmail.com>
2025-01-13 00:22:09 -0600

    update

--- components/ui/cards/Chat/ChatMessage/UserMessage.tsx
+++ components/ui/cards/Chat/ChatMessage/UserMessage.tsx
@@ -11,7 +11,7 @@ export const UserMessage: FC<UserMessageProps> = () => {
             <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-1">
                 <div className="flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2">
                     <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left text-slate-900">Username</p>
-                    <p className="flex-grow-0 flex-shrink-0 text-xs text-left text-slate-500">San Jose, CA</p>
+                    <p className="flex-grow-0 flex-shrink-0 text-xs text-left text-slate-500">Dallas, TX</p>
                     <p className="flex-grow-0 flex-shrink-0 text-xs text-left text-slate-500">12h</p>
                 </div>
                 <p className="self-stretch flex-grow-0 flex-shrink-0 w-[393px] text-sm text-left text-slate-900">Message</p>
